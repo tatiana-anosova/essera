b1844fb232104d00a495cb8e55a05b41c8d3f045
Tatiana Anosova <tanya.pmk@gmail.com>
2025-07-23 14:06:05 -0500

    Add new-arrivals page

--- src/app/new-arrivals/page.tsx
+++ src/app/new-arrivals/page.tsx
@@ -1,12 +1,18 @@
 import styles from './page.module.css';
 import clsx from 'clsx';
+import { mockProducts } from '@/data/products';
+import { ProductList } from '@/components';
 
 export default function NewArrivals() {
+	const productList = mockProducts.filter(product => product.label === 'new');
+
 	return (
 		<div className={clsx('page', styles.page)}>
 			<main className={clsx(styles.main)}>
 				<h1>New Arrivals</h1>
-
+				<div className={clsx(styles.list)}>
+					<ProductList products={productList}/>
+				</div>
 			</main>
 		</div>
 	);
